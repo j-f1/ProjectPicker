89e3a166616f0705a023911db98ce14129a5f7fd
Jed Fox <git@jedfox.com>
2021-12-25 17:00:03 -0500

    Sort by date, ignore packages

--- Sources/ProjectPicker/main.swift
+++ Sources/ProjectPicker/main.swift
@@ -3,12 +3,29 @@ import CoreText
 
 defer { try? Config.shared.save() }
 
-let projects = try Config.shared.searchPaths.flatMap { (path) -> [Project] in
-    let url = URL(fileURLWithPath: path, relativeTo: FileManager.default.homeDirectoryForCurrentUser)
-    let urls = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: [.isDirectoryKey], options: [])
-    return try urls
-        .filter { try $0.resourceValues(forKeys: [.isDirectoryKey]).isDirectory == true }
-        .map(Project.init)
+@objcMembers class ProjectWithDate: NSObject {
+    let project: Project
+    let date: Date
+    init(url: URL, date: Date?) throws  {
+        self.project = try Project(url)
+        self.date = date ?? .distantPast
+    }
 }
 
-try Alfred.Result(items: projects.map(\.alfredItem)).output(debug: false)
+let projects = try Config.shared.searchPaths.flatMap { (path) -> [ProjectWithDate] in
+    let url = URL(fileURLWithPath: path, relativeTo: FileManager.default.homeDirectoryForCurrentUser)
+    let urls = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: [.isDirectoryKey, .isPackageKey, .contentModificationDateKey], options: [])
+    return try urls
+        .filter { url in
+            let values = try url.resourceValues(forKeys: [.isDirectoryKey, .isPackageKey])
+            return values.isDirectory == true && values.isPackage != true
+        }
+        .map { url in
+            return try ProjectWithDate(
+                url: url,
+                date: try? url.resourceValues(forKeys: [.contentModificationDateKey]).contentModificationDate
+            )
+        }
+}.sorted(using: SortDescriptor(\ProjectWithDate.date, order: .reverse))
+
+try Alfred.Result(items: projects.map(\.project.alfredItem)).output(debug: false)
